39711e08b0754313c18ec2c4e4915ee69681f558
jxderulo <hanchenghan@gmail.com>
2020-11-03 13:05:50 +0800

    update code

--- check_data.py
+++ check_data.py
@@ -6,33 +6,46 @@ end_date = datetime.strptime("2020-10-08", "%Y-%m-%d")
 day_count = (end_date - start_date).days + 1
 
 
-def pass_args(date, table):
-    pass_args = {
+def start_crawl_data(date, table):
+    init_args = {
         'project': 'data_crawler',
-        'query_date': date
+        'spider': table['spider'],
+        'query_date': date,
     }
-    for arg in table:
-        pass_args[arg] = table[arg]
 
-    return pass_args
+    if 'serveral_args' in table:
+        args = table['serveral_args']
+        for arg in args:
+            request_args = get_request_args(init_args, arg)
+            print("request parameters: " + str(request_args))
+            # Crawldata(request_args)
+    else:
+        print("request parameters: " + str(init_args))
+        # Crawldata(init_args)
 
 
-# check table
-print('Start crawling at: ' + current_datetime)
-print('===========================')
-for table in table_spider_dict:
-    print("table: " + table)
-    table = table_spider_dict[table]
+def get_request_args(pass_arg, arg):
+    for a in arg:
+        pass_arg[a] = arg[a]
+    return pass_arg
 
-    # ------- to be implement --------
-    for single_date in [start_date + timedelta(n) for n in range(day_count)]:
-        if (single_date.isoweekday() != 6 and single_date.isoweekday() != 7):
-            single_date = strftime("%Y-%m-%d", single_date.timetuple())
 
-            # query table to get lost date
+if __name__ == '__main__':
+    # check table
+    print('Start crawling at: ' + current_datetime)
+    print('===========================')
+    for table in table_spider_dict:
+        print("table: " + table)
+        print('=======')
+        table = table_spider_dict[table]
+        spider = table['spider']
+        # ------- to be implement --------
+        for single_date in [start_date + timedelta(n) for n in range(day_count)]:
+            if (single_date.isoweekday() != 6 and single_date.isoweekday() != 7):
+                single_date = strftime("%Y-%m-%d", single_date.timetuple())
 
-            # if find lost date:
-            pass_parameters = pass_args(single_date, table)
-            print(pass_parameters)
-            Crawldata(pass_parameters)
-    # ------- to be implement --------
+                # query table to get lost date
+
+                # if find lost date:
+                start_crawl_data(single_date, table)
+        # ------- to be implement --------
